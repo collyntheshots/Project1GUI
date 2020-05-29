86f78868345e1390e8847837784c51cb9daed571
Collyn Lindley <51330857+collyntheshots@users.noreply.github.com>
2020-05-28 23:44:43 -0400

    Add files via upload

---
+++ Project/Project/test.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+public class test{
+
+     private static final int WINDOW_WIDTH = 410; //pixels
+     private static final int WINDOW_HEIGHT = 235; //pixels
+     private static final int FIELD_WIDTH = 20;   //characters
+     private static final int AREA_WIDTH = 40;   //characters
+
+     public test()
+     {
+      JFrame frame = new JFrame("My First GUI");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+      JButton press = new JButton("Press");
+      JButton newButton = new JButton("New");
+      frame.getContentPane().add(press); // Adds Button to content pane of frame
+      frame.getContentPane().add(newButton);
+      frame.setVisible(true);
+     }
+
+    public static void main(String args[]){
+
+      test gui = new test();
+      System.out.println("Hello, World!");
+    }
+}
