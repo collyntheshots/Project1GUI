1b14850d9548943052e9232b088d61fcd477d71b
Collyn Lindley <51330857+collyntheshots@users.noreply.github.com>
2020-05-31 01:34:15 -0400

    Add files via upload
    new inventory class
    window needs a method fixed

---
+++ Project/Inventory.java
@@ -0,0 +1,66 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class Inventory
+{
+  private static String DEFAULT_FILENAME = "inventory.txt";
+  private static String filename = "";
+  private static String [] currentItemInfo = new String [3];
+
+  public boolean isInInventory(String itemId)
+  {
+    String readLine = "";
+    try
+    {
+      BufferedReader br = new BufferedReader(new FileReader(this.filename));
+      while ((readLine = br.readLine()) != null)
+      {
+        String [] splitLine = readLine.split(",");
+
+        if (splitLine[0].equals(itemId))
+        {
+          for (int i = 0; i < splitLine.length; i++)
+            this.currentItemInfo[i] = splitLine[i];
+          return true;
+        }
+      }
+    }
+    catch (Exception e)
+    {
+      System.err.println(e);
+    }
+    return false;
+  }
+
+  public static String [] getCurrentItemInfo()
+  {
+    return currentItemInfo;
+  }
+  public Inventory(String filename)
+  {
+    this.filename = filename;
+  }
+
+  public Inventory()
+  {
+    this(DEFAULT_FILENAME);
+  }
+
+/*
+  public static void main(String [] args)
+  {
+    Inventory i = new Inventory();
+    if(i.isInInventory(1111))
+    {
+      System.out.println("you got it");
+      String [] item = getCurrentItemInfo();
+
+      for (int j = 0; j < item.length; j++)
+        System.out.println(item[j]);
+    }
+    else
+      System.out.println("Fail");
+  }
+*/
+}
--- Project/Window.java
+++ Project/Window.java
@@ -1,88 +1,161 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Window implements ActionListener{
+public class Window implements ActionListener
+{
+  private static final int WINDOW_WIDTH = 800; //pixels
+  private static final int WINDOW_HEIGHT = 235; //pixels
+  private static final int FIELD_WIDTH = 50;   //characters
+  private static final int AREA_WIDTH = 40;   //characters
 
-     private static final int WINDOW_WIDTH = 800; //pixels
-     private static final int WINDOW_HEIGHT = 235; //pixels
-     private static final int FIELD_WIDTH = 60;   //characters
-     private static final int AREA_WIDTH = 40;   //characters
+  private static final FlowLayout LAYOUT_STYLE = new FlowLayout();
 
-     private static final FlowLayout LAYOUT_STYLE = new FlowLayout();
+  private Inventory inventory;
 
-     // Creation of window
-     private JFrame window = new JFrame("My First GUI");
+  private int itemNum = 1;
+  private int numOfItems = 0;
+  //private String [][] itemData;
 
-     // Creation of text fields
-     private JLabel textTag = new JLabel("New text field");
-     private JTextField textField = new JTextField(FIELD_WIDTH);
+  // Creation of window
+  private JFrame window = new JFrame("Ye Old Book Shoppe");
 
-     private JLabel textTag2 = new JLabel("New text field 2");
-     private JTextField textField2 = new JTextField(FIELD_WIDTH);
+  // Creation of text fields
+  private JLabel numItemsTag = new JLabel("Enter the Number of Items");
+  private JTextField numItemsField = new JTextField(FIELD_WIDTH);
 
-     // Creation of buttons
-     private JButton procItem = new JButton("Process Item #1");
-     private JButton confirmItem = new JButton("Confirm Item #1");
-     private JButton viewOrder = new JButton("View Order");
-     private JButton finishOrder = new JButton("Finish Order");
-     private JButton newOrder = new JButton("New Order");
-     private JButton exit = new JButton("Exit");
+  private JLabel bookIdTag = new JLabel("             Book ID for item #" + itemNum);
+  private JTextField bookIdField = new JTextField(FIELD_WIDTH);
 
-     public test()
-     {
-      window.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
-      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Container c = window.getContentPane();
-      c.setLayout(LAYOUT_STYLE);
+  private JLabel quantityTag = new JLabel("           Quantity for item #" + itemNum);
+  private JTextField quantityField = new JTextField(FIELD_WIDTH);
 
-      // register event listeners
-      /*
-      procItem
-      confirmItem
-      viewOrder
-      finishOrder
-      newOrder
-      exit
-      */
+  private JLabel itemInfoTag = new JLabel("                          Item #" + itemNum + " info");
+  private JTextField itemInfoField = new JTextField(FIELD_WIDTH);
 
-      procItem.addActionListener(this);
-      confirmItem.addActionListener(this);
-      viewOrder.addActionListener(this);
-      finishOrder.addActionListener(this);
-      newOrder.addActionListener(this);
-      exit.addActionListener(this);
+  private JLabel subtotalTag = new JLabel("Order Subtotal for " + numOfItems + " items");
+  private JTextField subtotalField = new JTextField(FIELD_WIDTH);
 
-      c.add(textTag);
-      c.add(textField);
-      c.add(textTag2);
-      c.add(textField2);
-      c.add(procItem);
-      c.add(confirmItem);
-      c.add(viewOrder);
-      c.add(finishOrder);
-      c.add(newOrder);
-      c.add(exit);
+  // Creation of buttons
+  private JButton procItem = new JButton("Process Item #1");
+  private JButton confirmItem = new JButton("Confirm Item #1");
+  private JButton viewOrder = new JButton("View Order");
+  private JButton finishOrder = new JButton("Finish Order");
+  private JButton newOrder = new JButton("New Order");
+  private JButton exit = new JButton("Exit");
 
-      window.show();
-     }
+  public Window(Inventory i)
+  {
+    this.inventory = i;
+    //setItemData();
+    window.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-     public void actionPerformed(ActionEvent e)
-     {
-       String response1 = textField.getText();
-       textField2.setText(response1);
-       System.out.println(response1);
-     }
+    // register event listeners
+    procItem.addActionListener(this);
 
-    public static void main(String args[]){
+    confirmItem.setEnabled(false);
+    confirmItem.addActionListener(this);
 
-      try
+    viewOrder.setEnabled(false);
+    viewOrder.addActionListener(this);
+
+    finishOrder.setEnabled(false);
+    finishOrder.addActionListener(this);
+
+    newOrder.addActionListener(this);
+    exit.addActionListener(this);
+
+    // add buttons
+    Container c = window.getContentPane();
+    c.setLayout(LAYOUT_STYLE);
+
+    c.add(numItemsTag);
+    c.add(numItemsField);
+
+    c.add(bookIdTag);
+    c.add(bookIdField);
+
+    c.add(quantityTag);
+    c.add(quantityField);
+
+    c.add(itemInfoTag);
+    c.add(itemInfoField);
+
+    c.add(subtotalTag);
+    c.add(subtotalField);
+
+    c.add(procItem);
+    c.add(confirmItem);
+    c.add(viewOrder);
+    c.add(finishOrder);
+    c.add(newOrder);
+    c.add(exit);
+
+    window.setVisible(true);
+  }
+
+/*
+  private void setItemData()
+  {
+    //"22222"; "\"W Is For Wasted - Sue Grafton\", 9.95"
+    String [] item = {"22222", "11221122"};
+    itemData[0][0] = item[0];
+  }
+*/
+
+  public void actionPerformed(ActionEvent e)
+  {
+    if (e.getSource() == procItem)
+    {
+      // Line need fixing *********************************************************************************
+      if (inventory.isInInventory(bookIdField.getText()) && (Integer.parseInt(quantityField.getText()) > 0))
       {
-        test gui = new test();
+        itemNum++;
+        procItem.setText("Process Item #" + itemNum);
+        confirmItem.setEnabled(true);
+        procItem.setEnabled(false);
       }
-      catch(Exception e)
-      {
-        System.out.println(e);
-      }
-      System.out.println("Program ran successfully");
     }
+    else if (e.getSource() == confirmItem)
+    {
+      confirmItem.setEnabled(false);
+      procItem.setEnabled(true);
+    }
+    else if (e.getSource() == newOrder)
+    {
+      itemNum = 1;
+
+      numItemsField.setText("");
+      bookIdField.setText("");
+      quantityField.setText("");
+      itemInfoField.setText("");
+      subtotalField.setText("");
+
+      procItem.setText("Process Item #" + itemNum);
+      confirmItem.setText("Confirm Item #" + itemNum);
+      bookIdTag.setText("             Book ID for item #" + itemNum);
+      quantityTag.setText("           Quantity for item #" + itemNum);
+      itemInfoTag.setText("                          Item #" + itemNum + " info");
+      confirmItem.setEnabled(false);
+      procItem.setEnabled(true);
+    }
+    else if (e.getSource() == exit)
+    {
+      System.exit(0);
+    }
+  }
+
+  public static void main(String args[])
+  {
+    try
+    {
+      Inventory i = new Inventory();
+      Window gui = new Window(i);
+    }
+    catch(Exception e)
+    {
+      System.err.println(e);
+    }
+      System.out.println("Program ran successfully");
+  }
 }
